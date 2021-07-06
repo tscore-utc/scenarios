df9c80a42a0430213ee8219034903b5e5cb81132
Hayden Atchley <shaydenatch@gmail.com>
2021-07-05 21:12:15 -0600

    Update PopulationSampler.R to match folder structure

--- slc-tscore/microtransit_areas/R/Population_Sampler.R
+++ slc-tscore/microtransit_areas/R/Population_Sampler.R
@@ -1,22 +1,36 @@
-library(tidyverse)
-
-#Set WD [households, persons, trips, TAZ files must be in same dir]
-setwd("~/RA_Microtransit/files_to_kepp/activitysim_output")
-
-#Read in .csv's
-if(exists('allHouseholds') == FALSE) allHouseholds <- read_csv("final_households.csv")
-if(exists('allPersons') == FALSE) allPersons <- read_csv("final_persons.csv")
-if(exists('allTrips') == FALSE) allTrips <- read_csv("final_trips.csv")
-TAZinZone <- unname(deframe(read_csv("TAZ_in_Zone.csv")))
+##### User-entered Data #####
 
 #Set percent of people to sample
-pctInZone = 10
-pctOutZone = 1
+pctInZone = 0.1
+pctOutZone = 0.1
+
+#Set ridehail zone name [must match "TAZ in zone" file naming scheme]
+zoneName <- "slc_south"
 
 #Set output folder naming scheme
-outputFolder <- paste("SampleFiles_",pctInZone,"-",pctOutZone, sep = "")
+outputFolderName <- paste(zoneName, "_",pctInZone,"-",pctOutZone, sep = "")
 
-###############################################################################
+################################################################################
+##### Code #####
+
+library(tidyverse)
+
+script.dir <- dirname(sys.frame(1)$ofile)
+
+#Set WD for input files
+setwd(paste(script.dir, "/../", "input_CSVs/activitysim_output", sep = ''))
+
+#Set path of output folder
+outputFolder <- paste(script.dir, "/../", "input_CSVs/", outputFolderName, sep = '')
+
+#Read in CSVs
+if(exists('allHouseholds') == FALSE) allHouseholds <- read_csv("households_all.csv")
+if(exists('allPersons') == FALSE) allPersons <- read_csv("persons_all.csv")
+if(exists('allTrips') == FALSE) allTrips <- read_csv("trips_all.csv")
+
+#Read in TAZ in zone
+TAZInZoneFile <- paste(script.dir, "/../", "input_CSVs/TAZ_zones/TAZInZone_", zoneName, ".csv", sep = '')
+TAZinZone <- unname(deframe(read_csv(TAZInZoneFile)))
 
 #Add column to Households if in Zone
 allHouseholds['in_Zone'] <- deframe(allHouseholds['TAZ']) %in% TAZinZone
@@ -40,9 +54,8 @@ allTrips['in_Zone'] <- deframe(allTrips['household_id']) %in% deframe(sampleHous
 sampleTrips <- split(allTrips,allTrips['in_Zone'])$`TRUE` %>% arrange(person_id,depart)
 
 
-
 #Create output directory and write files
-dir.create(outputFolder)
-write_csv(sampleHouseholds, paste(getwd(),"/",outputFolder,"/households.csv", sep = ""))
-write_csv(samplePersons, paste(getwd(),"/",outputFolder,"/persons.csv", sep = ""))
-write_csv(sampleTrips, paste(getwd(),"/",outputFolder,"/trips.csv", sep = ""))
+if(dir.exists(outputFolder) == FALSE) dir.create(outputFolder)
+write_csv(sampleHouseholds, paste(outputFolder, "/households.csv", sep = ''))
+write_csv(samplePersons, paste(outputFolder, "/persons.csv", sep = ''))
+write_csv(sampleTrips, paste(outputFolder, "/trips.csv", sep = ''))
